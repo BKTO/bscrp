53ada71445452678d17cec8eee0ef8fc4552f3b4
BKTO <hello@betterknowtheopposition.com>
2016-06-24 04:27:17 +0000

    rewrote anonymize

--- anonymize.sh
+++ anonymize.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-git filter-branch --env-filter '
-CORRECT_NAME="BKTO"
-CORRECT_EMAIL="hello@betterknowtheopposition.com"
-export GIT_COMMITTER_NAME="$CORRECT_NAME"
-export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
-export GIT_AUTHOR_NAME="$CORRECT_NAME"
-export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
-' --tag-name-filter cat -- --branches --tags --force
+git filter-branch -f --env-filter '
+export GIT_COMMITTER_NAME="BKTO"
+export GIT_COMMITTER_EMAIL="hello@betterknowtheopposition.com"
+export GIT_AUTHOR_NAME="BKTO"
+export GIT_AUTHOR_EMAIL="hello@betterknowtheopposition.com"
+' master
 
 git push --force --tags origin 'refs/heads/*'
